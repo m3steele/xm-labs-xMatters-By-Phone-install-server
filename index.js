0cc9733af8a904d518cb3956463f43a4e306abb1
msteelebucket <msteele@xmatters.com>
2021-08-31 13:17:26 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//Load HTTP module
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/installfunctions', function (req, res) {
+  res.send('Installing Twilio Function Versions');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
