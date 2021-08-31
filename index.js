7e723ff5e4e6c2b196b88e042b9ea3e0d3d8880a
msteelebucket <msteele@xmatters.com>
2021-08-31 13:49:28 -0700

    PORT as upper case

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Load HTTP module
 const express = require('express');
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
